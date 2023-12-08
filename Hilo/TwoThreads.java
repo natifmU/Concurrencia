2e205fccbdb951569261edf11ebf9537a0f69ffc
Natalia Fajardo Marzolla <150955864+natifmU@users.noreply.github.com>
2023-12-08 20:16:25 +0100

    Update TwoThreads.java

--- Hilo/TwoThreads.java
+++ Hilo/TwoThreads.java
@@ -4,17 +4,15 @@ public class TwoThreads implements Runnable {
 	//@Override
 	public void run() {
 		for (int i = 0; i < 5; i++) {
-			//System.out.println(i + " " + Thread.currentThread().getName());
-			//System.out.println("End of Thread" +  Thread.currentThread().getName());
-			System.out.println("Index: " + i + " Thread Name: " + Thread.currentThread().getName());
+			System.out.println("Food type: " + i + " Thread Name: " + Thread.currentThread().getName());
 		}
 	}
 	
 	public static void main(String[] args) {
-		new Thread(new TwoThreads(), "User 1").start();
-		new Thread(new TwoThreads(), "USer 2").start();
-		new Thread(new TwoThreads(), "User 3").start();
-		new Thread(new TwoThreads(), "User 4").start();
+		new Thread(new TwoThreads(), "Italian").start();
+		new Thread(new TwoThreads(), "Spanish").start();
+		new Thread(new TwoThreads(), "Chinese").start();
+		new Thread(new TwoThreads(), "Japanese").start();
 		System.out.println("End of Main");
 	}
 
