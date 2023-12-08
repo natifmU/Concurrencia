6693a499c99e2d191a0587872f76eb4663b8f7e5
Natalia Fajardo Marzolla <150955864+natifmU@users.noreply.github.com>
2023-12-08 20:30:49 +0100

    Update TwoThreads.java

--- Hilo/TwoThreads.java
+++ Hilo/TwoThreads.java
@@ -4,7 +4,7 @@ public class TwoThreads implements Runnable {
 	//@Override
 	public void run() {
 		for (int i = 0; i < 5; i++) {
-			System.out.println("Selection: " + i + " Food type: " + Thread.currentThread().getName());
+			System.out.println("User: " + i + " Food type: " + Thread.currentThread().getName());
 		}
 	}
 	
